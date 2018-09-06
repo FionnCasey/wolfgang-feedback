98424fed406d426d746b2768373b4e17dc5333a4
Fionn Casey <fionn.casey@ad.wolfgangdigital.com>
2018-09-06 10:38:56 +0100

    Fixes

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (req, res) => res.status(200).send('Awarewolf Beta'));
+
 // Middleware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
